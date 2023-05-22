3703986232fba35a49111e2313b397c5c21cfdd3
Safakan <80333460+Safakan@users.noreply.github.com>
2023-05-22 11:42:46 +0300

    Organising the files
    Moved the refactored version to a clean folder.
    Removed the tests
    Started preparing the final readme file.

--- tests/test_imports.py
+++
@@ -1,9 +0,0 @@
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
-
-
-print(3)
